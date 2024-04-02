35c72c25c2fb1f18b1009ae045e136f04bf6f3d2
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2024-04-02 13:56:43 -0300

    feat: remove specific quiz route

--- src/server.ts
+++ src/server.ts
@@ -9,21 +9,8 @@ app.use(cors());
 
 
 app.get('/quizzes', (req, res) => {
-  res.json(quizzesData.quizzes);
+  res.json(quizzesData);
 });
 
-
-app.get('/quizzes/:quizTitle', (req, res) => {
-  const { quizTitle } = req.params;
-  const quizTitleLower = quizTitle.toLowerCase();
-
-  const quiz = quizzesData.quizzes.find(q => q.title.toLowerCase() === quizTitleLower);
-
-  if (!quiz) {
-    res.status(404).json({ error: 'Quiz not found' });
-  } else {
-    res.json(quiz);
-  }
-});
 
 app.listen(3333)
